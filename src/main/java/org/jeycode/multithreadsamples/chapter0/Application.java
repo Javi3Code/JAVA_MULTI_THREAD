f8b3169808cb42684987c005fa4dce668dc71ca2
@JeyCode <j.code.initialize@gmail.com>
2021-03-01 12:40:17 +0100

    Comenzamos serie de Programación Multihilo en YT

---
+++ src/main/java/org/jeycode/multithreadsamples/chapter0/Application.java
@@ -0,0 +1,11 @@
+package org.jeycode.multithreadsamples.chapter0;
+
+public final class Application
+{
+
+      public static void main(String[] args)
+      {
+            // Diferente formas de crear un Thread
+      }
+
+}
